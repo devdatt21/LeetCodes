dd56d591342d2e42250f77c59b03f235bfe8f0d6
Devdatt Rupapara <78930215+devdatt21@users.noreply.github.com>
2024-09-28 22:41:44 +0530

    Time: 26 ms (10.20%) | Memory: 23 MB (11.65%) - LeetSync

--- 859-design-circular-deque/design-circular-deque.cpp
+++ 859-design-circular-deque/design-circular-deque.cpp
@@ -1,61 +1,127 @@
-#include <vector>
-
-class MyCircularDeque {
-private:
-    std::vector<int> data;
-    int front;
-    int rear;
-    int capacity;
-    int count;
+class Node {
 
 public:
-    MyCircularDeque(int k) : data(k), front(0), rear(k - 1), capacity(k), count(0) {}
-    
-    bool insertFront(int value) {
-        if (isFull()) return false;
-        front = (front - 1 + capacity) % capacity;
-        data[front] = value;
-        count++;
-        return true;
+    int value;
+    Node* next;
+    Node* prev;
+
+    Node() {
+        this->value = 0;
+        next = nullptr;
+        prev = nullptr;
     }
-    
-    bool insertLast(int value) {
-        if (isFull()) return false;
-        rear = (rear + 1) % capacity;
-        data[rear] = value;
-        count++;
-        return true;
-    }
-    
-    bool deleteFront() {
-        if (isEmpty()) return false;
-        front = (front + 1) % capacity;
-        count--;
-        return true;
-    }
-    
-    bool deleteLast() {
-        if (isEmpty()) return false;
-        rear = (rear - 1 + capacity) % capacity;
-        count--;
-        return true;
-    }
-    
-    int getFront() {
-        if (isEmpty()) return -1;
-        return data[front];
-    }
-    
-    int getRear() {
-        if (isEmpty()) return -1;
-        return data[rear];
-    }
-    
-    bool isEmpty() {
-        return count == 0;
-    }
-    
-    bool isFull() {
-        return count == capacity;
+
+    Node(int value) {
+        this->value = value;
+        next = nullptr;
+        prev = nullptr;
     }
 };
+
+class MyCircularDeque {
+public:
+    int size = 0;
+    Node* front;
+    Node* rear;
+    int cnt = 0;
+
+    MyCircularDeque(int k) { 
+        size = k; 
+        front = new Node();
+        rear = new Node();
+
+        front->next=rear;
+        rear->prev=front;
+    }
+
+    bool insertFront(int value) {
+
+        if (cnt == size)
+            return false;
+        Node* n = new Node(value);
+        n->next = front->next;
+        n->prev = front;
+        front->next->prev = n;
+        front->next = n;
+        cnt++;
+
+        return true;
+    }
+
+    bool insertLast(int value) {
+        if (cnt == size)
+            return false;
+
+        Node* n = new Node(value);
+        n->next = rear;
+        n->prev = rear->prev;
+        rear->prev->next = n;
+        rear->prev = n;
+        cnt++;
+
+        return true;
+    }
+
+    bool deleteFront() {
+        if (cnt == 0)
+            return false;
+        Node* n = front->next;
+        front->next = front->next->next;
+        front->next->prev = front;
+        cnt--;
+
+        delete n;
+        return true;
+    }
+
+    bool deleteLast() {
+        if (cnt == 0)
+            return false;
+        Node* n = rear->prev;
+        rear->prev = rear->prev->prev;
+        rear->prev->next = rear;
+        cnt--;
+
+        delete n;
+        return true;
+    }
+
+    int getFront() {
+        if (cnt == 0)
+            return -1;
+        return front->next->value;
+    }
+
+    int getRear() {
+        if (cnt == 0)
+            return -1;
+        return rear->prev->value;
+    }
+
+    bool isEmpty() {
+        if (cnt == 0)
+            return true;
+        else
+            return false;
+    }
+
+    bool isFull() {
+        if (cnt == size)
+            return true;
+        else
+            return false;
+    }
+};
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * MyCircularDeque* obj = new MyCircularDeque(k);
+ * bool param_1 = obj->insertFront(value);
+ * bool param_2 = obj->insertLast(value);
+ * bool param_3 = obj->deleteFront();
+ * bool param_4 = obj->deleteLast();
+ * int param_5 = obj->getFront();
+ * int param_6 = obj->getRear();
+ * bool param_7 = obj->isEmpty();
+ * bool param_8 = obj->isFull();
+ */
