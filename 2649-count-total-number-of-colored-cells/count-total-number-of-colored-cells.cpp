eaab16ebcbb5754c6c46f37a75ea258a0ef69965
Devdatt Rupapara <78930215+devdatt21@users.noreply.github.com>
2025-03-05 11:38:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (61.42%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
-    long long coloredCells(int n) {
+    long long coloredCells(long long n) {
         // n=1 : 1
         // n=2 : 5 
         // n=3 : 13 5 + 4 + (4*cnt:1)
         // n=4 : 25 13 + 4 + (4*cnt:2)
         // n=5 : 41
-
+// 5 = my m
         // long long cnt = 
 
-        if(n==1) return 1;
-        if(n==2) return 5;
-
-        long long cnt = 1;
-        vector<long long> vec(n+1,0);
-        vec[1]=1;
-        vec[2]=5;
-
-        for(int i=3;i<=n;i++)
-        {
-            vec[i]=vec[i-1] + 4 + (4*cnt++);
-        }
-
-        return vec[n];
+        return n * (n-1) * 2 + 1LL;
     }
 };
