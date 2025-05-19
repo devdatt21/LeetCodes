68f2cac3d15ff7132f3f199b4e1924d60d6c13c5
Devdatt Rupapara <78930215+devdatt21@users.noreply.github.com>
2025-05-19 22:31:37 +0530

    Time: 0 ms (100.00%) | Memory: 4 MB (100.00%) - LeetSync

---
+++ 3321-type-of-triangle/type-of-triangle.go
@@ -0,0 +1,16 @@
+func triangleType(nums []int) string {
+    sort.Ints(nums);
+
+    if nums[2] >= nums[0]+nums[1]{
+        return "none"
+    }
+
+    if nums[0] == nums[1] && nums[1] == nums[2]{
+        return "equilateral"
+
+    } else if nums[0] == nums[1] || nums[1] == nums[2] || nums[0] == nums[2]{
+        return "isosceles"
+    } else {
+        return "scalene"
+    } 
+}
