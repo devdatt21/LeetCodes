7dda399e2118fcdcafd3c7711bf592ae307da111
Devdatt Rupapara <78930215+devdatt21@users.noreply.github.com>
2025-07-01 22:38:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (29.69%) - LeetSync

---
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int cnt=1;
+        for(int i=1;i<word.size();i++)
+        {
+            if(word[i]==word[i-1])
+            {
+                cnt++;
+            }
+        }
+        // cnt++;
+
+        return cnt;
+    }
+};
